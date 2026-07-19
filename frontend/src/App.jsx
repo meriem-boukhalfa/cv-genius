e152f8090557c9eeed73a9a6525e531a0f50857c
Boukhalfa Meriem <boukhalfa2012@gmail.com>
2026-07-19 18:02:09 +0100

    Add professional footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 
+import Footer from "./components/Footer";
+
 import CreateResume from "./pages/CreateResume";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<CreateResume />} />
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      <Route path="/terms-of-service" element={<TermsOfService />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<CreateResume />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
